a6b73873a5562f788c1f5eeb153898a97dfe194c
Thomas Hornschuh <thomas@hornschuh-family.de>
2016-08-08 17:15:19 +0200

    changed main.c

--- software/hello/main.c
+++ software/hello/main.c
@@ -8,8 +8,10 @@
 
 #define PLATFORM_UART0_BASE 0xc0002000
 #define PLATFORM_SYSCLK_FREQ 32000000
+#define PLATFORM_GPIO_BASE 0xc0004000
 
 #include "uart.h"
+#include "gpio.h"
 
 void exception_handler(uint32_t cause, void * epc, void * regbase)
 {
@@ -17,19 +19,27 @@ void exception_handler(uint32_t cause, void * epc, void * regbase)
 }
 
 static struct uart uart0;
+static struct gpio gpio0;
+
 
 int main(void)
 {
 	const char * hello_string = "Hello world\n\r";
+        
+        gpio_initialize(&gpio0,(volatile void *)PLATFORM_GPIO_BASE);
+        gpio_set_direction(&gpio0,0b11110000);
+        gpio_set_output(&gpio0,0b10010000);  // Display 1001 at the LEDs
 
 	uart_initialize(&uart0, (volatile void *) PLATFORM_UART0_BASE);
-	uart_set_divisor(&uart0, uart_baud2divisor(115200, PLATFORM_SYSCLK_FREQ));
+	uart_set_divisor(&uart0, uart_baud2divisor(9600, PLATFORM_SYSCLK_FREQ));
 
-	for(int i = 0; hello_string[i] != 0; ++i)
-	{
+        do {
+	  for(int i = 0; hello_string[i] != 0; ++i)
+	  {
 		while(uart_tx_fifo_full(&uart0));
 		uart_tx(&uart0, hello_string[i]);
-	}
+	  }
+        }while(true);
 
 	return 0;
 }
